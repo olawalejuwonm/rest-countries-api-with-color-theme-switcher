68ba95158f393e6d0e13f0bd7ab82ff31e3545f5
olawale micheal juwon <olawalejuwon@gmail.com>
2020-09-22 17:48:08 +0100

    done

--- scripts/main.js
+++ scripts/main.js
@@ -124,6 +124,62 @@ const FilAmerica = () => {
     });
 }
 
+const FilEurope = () => {
+    let Countries = [];
+    mainData.map((datas) => {
+        datas.map((data) => {
+            if ((data.region.toLowerCase()) === "europe") {
+                Countries.push(data)
+            }
+        })
+    });
+    Hresp.innerHTML = ""
+    Countries.map((res) => {
+        const temp = `<div class="card shadow mb-5" style="height:25rem" >
+                <img src="${res.flag}" class="card-img-top" alt="${res.name}" >
+                <div class="card-body mt-md-5">
+                <p class="card-title font-weight-bolder ">${res.name}</p>
+                <p class="font-weight-light p-0 m-0">Population: <span class="card-text ">${res.population}</span></p>
+                <p class="font-weight-light p-0 m-0">Region: <span class="card-text ">${res.region}</span></p>
+                <p class="font-weight-light p-0 m-0">Capital: <span class="card-text ">${res.capital}</span></p>
+                </div>
+              </div>`
+        const wDiv = document.createElement("div");
+        wDiv.setAttribute("class", "col-12 col-md-3");
+        wDiv.innerHTML = temp
+        Hresp.appendChild(wDiv)
+    });
+}
+
+const FilOceania = () => {
+    let Countries = [];
+    mainData.map((datas) => {
+        datas.map((data) => {
+            if ((data.region.toLowerCase()) === "oceania") {
+                Countries.push(data)
+            }
+        })
+    });
+    Hresp.innerHTML = ""
+    Countries.map((res) => {
+        const temp = `<div class="card shadow mb-5" style="height:25rem" >
+                <img src="${res.flag}" class="card-img-top" alt="${res.name}" >
+                <div class="card-body mt-md-5">
+                <p class="card-title font-weight-bolder ">${res.name}</p>
+                <p class="font-weight-light p-0 m-0">Population: <span class="card-text ">${res.population}</span></p>
+                <p class="font-weight-light p-0 m-0">Region: <span class="card-text ">${res.region}</span></p>
+                <p class="font-weight-light p-0 m-0">Capital: <span class="card-text ">${res.capital}</span></p>
+                </div>
+              </div>`
+        const wDiv = document.createElement("div");
+        wDiv.setAttribute("class", "col-12 col-md-3");
+        wDiv.innerHTML = temp
+        Hresp.appendChild(wDiv)
+    });
+}
+
+
+
 // const gggh = "mic"
 
 // console.log(gggh.includes("mi"))
